7da97438633caa630cebec4d59e978b2457df474
Utkarsh Karki <utkarsh1998@icloud.com>
2024-05-30 10:17:17 -0700

    RoundedCornerShape -- clipped cards

--- app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -11,8 +11,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -44,8 +47,10 @@ class MainActivity : ComponentActivity() {
 fun FirstApp(modifier: Modifier) {
     Surface(
         modifier = modifier
-            .fillMaxSize()
-            .border(2.dp, Color.Red)
+            .height(150.dp).width(400.dp),
+//            .border(2.dp, Color.Red),
+        color = Color.Green,
+        shape = RoundedCornerShape(28.dp, 28.dp, 28.dp, 28.dp)
     ) {
         Column {
             MyText(text = "My First App Practice", fontSize = 32.sp, modifier = Modifier.padding(start = 10.dp))
